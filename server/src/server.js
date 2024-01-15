bb60e52e2cdbba09f732ef272e5edf55c1e91b8d
dev-shubhamsoni <dev.shubhamsoni@gmail.com>
2024-01-15 21:11:35 +0530

    adding files

--- server/src/server.js
+++ server/src/server.js
@@ -7,7 +7,7 @@ const { mongoConnect } = require('./services/mongo');
 const { loadPlanetsData } = require('./models/planets.model');
 const { loadLaunchData } = require('./models/launches.model');
 
-const PORT = process.env.PORT || 8000;
+const PORT = 4002;
 
 const server = http.createServer(app);
 
